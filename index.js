4ce6dd2b4539cd4c74433247d9b9280b0142d34c
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-06-18 15:03:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+  
+
+  countDown(10);
